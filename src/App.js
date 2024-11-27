a9ea9319da24fd2f9d91243371593ac2ddb1b536
JulioHTML <bguille410@gmail.com>
2024-11-27 10:47:43 -0300

    ayuda jesus

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
-  const [category, setCategory] = useState("");
+  const [category, setCategory] = useState('');
+  const[selectCategory, setselectCategory] = useState(['electronics', 'jewelery', "men's clothing", "women's clothing"])
+  const [products, setproducts] = useState([]);
+  const[productsCategory, setproductsCategory] = useState([]);
+  const [errorMessage, seterrorMessage]= useState('')
+
+  useEffect(() => {
+    axios.get(`https://fakestoreapi.com/products`)
+      .then((res) => {
+        if (!res.ok) { setproducts(res.data); }
+        else { throw new Error(`${res.status}`) }
+      })
+      .catch(error => seterrorMessage(error.message));
+  }, [])
+
+  const enviar = () => {
+    axios.get(`https://fakestoreapi.com/products/${category}`)
+    .then ((res)=>{
+      if(!res.ok){setproductsCategory(res.data)}
+      else{throw new Error(`${res.status}`)}
+    })
+    .catch(error => seterrorMessage(error.message))
+    console.log(productsCategory)
+  }
 
   const handleCategoryChange = (event) => setCategory(event.target.value)
+
+  const selectedCategory = (e) =>{
+
+  }
+
+  console.log(products)
 
   return (
     <main>
@@ -13,23 +43,42 @@ function App() {
 
       <div>
         <h2>Lista de todos los productos disponibles:</h2>
-        <p className="result-box"></p>
+        <ul>
+        {products.map((product) =>{
+          console.log(product.id);
+          <li key={product.id}>{product.title}</li>/*, <p>{product.price}</p>*/
+        })}
+        </ul>
+        <p className="result-box">
+
+          
+        </p>
       </div>
 
       <div>
         <h2>Obtener productos de una categoría determinada</h2>
 
         <h3>Ingrese una categoría:</h3>
+        <select onChange={selectedCategory}>
+          {selectCategory.map((cat)=>{
+            <option value={cat}>{cat}</option>
+          })}
+        </select>
         <input type="text" value={category} onChange={handleCategoryChange} />
-        <button>Enviar</button>
-        
+        <button onClick={enviar}>Enviar</button>
+
         <h3>Productos de la categoría ingresada:</h3>
-        <p className="result-box"></p>
+        <ul className="result-box">
+        {productsCategory.map((product) =>{
+          <li key={product.id}>{product.title}</li>/*, <p>{product.price}</p>*/
+        })}
+        </ul>
+
       </div>
 
       <div>
         <h2>Mensaje en caso de error:</h2>
-        <p className="result-box"></p>
+        <p className="result-box">{errorMessage}</p>
       </div>
 
       <div>
